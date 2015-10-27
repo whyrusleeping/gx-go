2d97be185162e1f5a975e06c03cb85f7cd424332
Jeromy <jeromyj@gmail.com>
2015-10-26 22:27:24 -0700

    implement path rewriting

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
+	"go/ast"
 	"go/build"
+	"go/parser"
+	"go/printer"
+	"go/token"
 	"log"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
+	"strconv"
 	"strings"
 
 	gx "github.com/whyrusleeping/gx/gxutil"
@@ -49,9 +56,10 @@ func pathIsNotStdlib(path string) bool {
 }
 
 type Importer struct {
-	pkgs   map[string]string
-	gopath string
-	pm     *gx.PM
+	pkgs    map[string]*gx.Dependency
+	gopath  string
+	pm      *gx.PM
+	rewrite bool
 }
 
 func NewImporter(rw bool) (*Importer, error) {
@@ -61,9 +69,10 @@ func NewImporter(rw bool) (*Importer, error) {
 	}
 
 	return &Importer{
-		pkgs:   make(map[string]string),
-		gopath: gp,
-		pm:     gx.NewPM(),
+		pkgs:    make(map[string]*gx.Dependency),
+		gopath:  gp,
+		pm:      gx.NewPM(),
+		rewrite: rw,
 	}, nil
 }
 
@@ -75,15 +84,15 @@ func getGoPath() (string, error) {
 	return gopath, nil
 }
 
-func (i *Importer) GxPublishGoPackage(imppath string) (hash string, err error) {
-	if hash, ok := i.pkgs[imppath]; ok {
-		return hash, nil
+func (i *Importer) GxPublishGoPackage(imppath string) (*gx.Dependency, error) {
+	if d, ok := i.pkgs[imppath]; ok {
+		return d, nil
 	}
 
 	// make sure its local
-	err = GoGet(imppath)
+	err := GoGet(imppath)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	pkgpath := path.Join(i.gopath, "src", imppath)
@@ -91,7 +100,7 @@ func (i *Importer) GxPublishGoPackage(imppath string) (hash string, err error) {
 	pkg, err := gx.LoadPackageFile(pkgFilePath)
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return "", err
+			return nil, err
 		}
 
 		// init as gx package
@@ -99,19 +108,19 @@ func (i *Importer) GxPublishGoPackage(imppath string) (hash string, err error) {
 		pkgname := parts[len(parts)-1]
 		err = gx.InitPkg(pkgpath, pkgname, "go")
 		if err != nil {
-			return "", err
+			return nil, err
 		}
 
 		pkg, err = gx.LoadPackageFile(pkgFilePath)
 		if err != nil {
-			return "", err
+			return nil, err
 		}
 	}
 
 	// recurse!
 	gopkg, err := build.Import(imppath, "", 0)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	var depsToVendor []string
@@ -124,33 +133,126 @@ func (i *Importer) GxPublishGoPackage(imppath string) (hash string, err error) {
 
 	for n, child := range depsToVendor {
 		fmt.Printf("- processing dep %s for %s [%d / %d]\n", child, imppath, n+1, len(depsToVendor))
-		childhash, err := i.GxPublishGoPackage(child)
+		childdep, err := i.GxPublishGoPackage(child)
 		if err != nil {
-			return "", err
+			return nil, err
 		}
 
-		chnameParts := strings.Split(child, "/")
-
-		pkg.Dependencies = append(pkg.Dependencies, &gx.Dependency{
-			Hash: childhash,
-			Name: chnameParts[len(chnameParts)-1],
-		})
+		pkg.Dependencies = append(pkg.Dependencies, childdep)
 	}
 
 	err = gx.SavePackageFile(pkg, pkgFilePath)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	hash, err = i.pm.PublishPackage(pkgpath, pkg)
+	if i.rewrite {
+		err = i.rewriteImports(pkgpath)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	hash, err := i.pm.PublishPackage(pkgpath, pkg)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	fmt.Printf("published %s as %s\n", imppath, hash)
 
-	i.pkgs[imppath] = hash
-	return hash, nil
+	dep := &gx.Dependency{
+		Hash: hash,
+		Name: pkg.Name,
+	}
+	i.pkgs[imppath] = dep
+	return dep, nil
+}
+
+func (i *Importer) rewriteImports(pkgpath string) error {
+	fullpkgpath, err := filepath.Abs(pkgpath)
+	if err != nil {
+		return err
+	}
+	return filepath.Walk(pkgpath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		rel := path[len(fullpkgpath)+1:]
+
+		if strings.HasPrefix(rel, "vendor") {
+			return nil
+		}
+
+		if strings.HasPrefix(rel, ".git") {
+			return nil
+		}
+
+		if strings.HasSuffix(path, ".go") {
+			err := i.rewriteImportsInFile(path)
+			if err != nil {
+				fmt.Println("ERROR: ", err)
+				return err
+			}
+		}
+
+		return nil
+	})
+}
+
+// inspired by godeps rewrite, rewrites import paths with gx vendored names
+func (i *Importer) rewriteImportsInFile(fi string) error {
+	cfg := &printer.Config{Mode: printer.UseSpaces | printer.TabIndent, Tabwidth: 8}
+	fset := token.NewFileSet()
+	file, err := parser.ParseFile(fset, fi, nil, parser.ParseComments)
+	if err != nil {
+		return err
+	}
+
+	var changed bool
+	for _, imp := range file.Imports {
+		p, err := strconv.Unquote(imp.Path.Value)
+		if err != nil {
+			return err
+		}
+
+		dep, ok := i.pkgs[p]
+		if !ok {
+			continue
+		}
+
+		changed = true
+		imp.Path.Value = strconv.Quote(dep.Hash + "/" + dep.Name)
+	}
+
+	if !changed {
+		return nil
+	}
+
+	var buffer bytes.Buffer
+	if err = cfg.Fprint(&buffer, fset, file); err != nil {
+		return err
+	}
+	fset = token.NewFileSet()
+	file, err = parser.ParseFile(fset, fi, &buffer, parser.ParseComments)
+	ast.SortImports(fset, file)
+	wpath := fi + ".temp"
+	w, err := os.Create(wpath)
+	if err != nil {
+		return err
+	}
+	if err = cfg.Fprint(w, fset, file); err != nil {
+		return err
+	}
+	if err = w.Close(); err != nil {
+		return err
+	}
+
+	return os.Rename(wpath, fi)
 }
 
 // TODO: take an option to grab packages from local GOPATH
