c20360348b7e809a2650026d631a6d5b0375af8a
Lucas Molas <schomatis@gmail.com>
2018-08-26 11:50:08 -0300

    link: use the vendor dir instead of the global workspace
    (This patch changes the current API.)
    
    The previous behavior of `gx-go link` linked packages to the global workspace in
    `$GOPATH/gx/ipfs/` and affected any packages that would depend on it.
    
    Use the Go vendor directory to link packages locally. That implies an explicit
    parent-dependency relationship where a dependency package is now linked to the
    vendor directory of a particular parent package (instead of linking it to any
    package that might depended on that dependency specific hash). Using the vendor
    directory allows the import paths to remain the same (Go will look in that
    directory before looking in the `$GOPATH` where the global Gx workspace is
    located).

--- link.go
+++ link.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -19,7 +20,16 @@ var LinkCommand = cli.Command{
 	Usage: "Symlink packages to their dvcsimport repos, for local development.",
 	Description: `gx-go link eases local development by symlinking actual workspace repositories on demand.
 
+Links the specified dependecy package (by name or hash)
+to the parent package in the CWD. The link is done through
+the vendor/ directory.
+
 Example workflow:
+
+> cd $GOPATH/src/github.com/ipfs/go-ipfs
+> gx-go link go-unixfs
+linked QmQA5mdxru8Bh6dpC9PJfSkumqnmHgJX7knxSgBo5Lpime /home/user/go/src/github.com/libp2p/go-libp2p
+
 
 > gx-go link QmQA5mdxru8Bh6dpC9PJfSkumqnmHgJX7knxSgBo5Lpime QmVGtdTZdTFaLsaj2RwdVG8jcjNNcp1DE914DKZ2kHmXHw QmSpJByNKFX1sCsHBEp3R73FL4NF6FnQTEGyNAXHm2GS52
 linked QmQA5mdxru8Bh6dpC9PJfSkumqnmHgJX7knxSgBo5Lpime /home/user/go/src/github.com/libp2p/go-libp2p
@@ -53,21 +63,43 @@ unlinked QmVGtdTZdTFaLsaj2RwdVG8jcjNNcp1DE914DKZ2kHmXHw /home/user/go/src/github
 			Name:  "a,all",
 			Usage: "Remove all existing symlinks and reinstate the gx packages. Use with -r.",
 		},
+		cli.StringFlag{
+			Name:  "p,parent",
+			Usage: "Specify the path of the parent package of the linked dependency (default: CWD).",
+		},
 	},
 	Action: func(c *cli.Context) error {
 		remove := c.Bool("remove")
 		all := c.Bool("all")
+		parentPackagePath := c.String("parent")
 
-		hashes := c.Args()[:]
-		if len(hashes) == 0 {
-			links, err := listLinkedPackages()
+
+		if parentPackagePath == "" {
+			var err error
+			parentPackagePath, err = os.Getwd()
+			if err != nil {
+				return fmt.Errorf("error retrieving the current working directory for the parent package path: %s", err)
+			}
+		}
+
+		parentPkg, err := LoadPackageFile(filepath.Join(parentPackagePath, gx.PkgFileName))
+		if err != nil {
+			return fmt.Errorf("parent package not found in %s: %s",
+				parentPackagePath, err)
+		}
+
+		depRefs := c.Args()[:]
+		// It can either be a hash or a name.
+
+		if len(depRefs) == 0 {
+			links, err := listLinkedPackages(parentPackagePath)
 			if err != nil {
 				return err
 			}
 
 			if remove && all {
 				for _, link := range links {
-					hashes = append(hashes, link[0])
+					depRefs = append(depRefs, link[0])
 				}
 			}
 
@@ -78,28 +110,25 @@ unlinked QmVGtdTZdTFaLsaj2RwdVG8jcjNNcp1DE914DKZ2kHmXHw /home/user/go/src/github
 				return nil
 			}
 		}
-		pkg, _ := LoadPackageFile(gx.PkgFileName)
 
-		for _, hash := range hashes {
-			if pkg != nil {
-				// try to resolve
-				if dep := pkg.FindDep(hash); dep != nil {
-					hash = dep.Hash
-				}
+		for _, ref := range depRefs {
+			dep := parentPkg.FindDep(ref)
+			if dep == nil {
+				return fmt.Errorf("dependency reference not found in the parent package: %s", ref)
 			}
 
 			if remove {
-				target, err := unlinkPackage(hash)
+				target, err := unlinkDependency(dep, parentPackagePath)
 				if err != nil {
 					return err
 				}
-				fmt.Printf("unlinked %s %s\n", hash, target)
+				fmt.Printf("unlinked %s %s\n", dep.Name, target)
 			} else {
-				target, err := linkPackage(hash)
+				target, err := linkDependency(dep, parentPackagePath)
 				if err != nil {
 					return err
 				}
-				fmt.Printf("linked %s %s\n", hash, target)
+				fmt.Printf("linked %s %s\n", dep.Name, target)
 			}
 		}
 
@@ -107,14 +136,13 @@ unlinked QmVGtdTZdTFaLsaj2RwdVG8jcjNNcp1DE914DKZ2kHmXHw /home/user/go/src/github
 	},
 }
 
-func listLinkedPackages() ([][]string, error) {
+// TODO: Make this function work at the `Package` abstraction level,
+// independent of the hashes.
+func listLinkedPackages(parentPackagePath string) ([][]string, error) {
+
 	var links [][]string
 
-	srcdir, err := gx.InstallPath("go", "", true)
-	if err != nil {
-		return links, err
-	}
-	gxbase := filepath.Join(srcdir, "gx", "ipfs")
+	gxbase := filepath.Join(parentPackagePath, "vendor", "gx", "ipfs")
 
 	filepath.Walk(gxbase, func(path string, fi os.FileInfo, err error) error {
 		relpath, err := filepath.Rel(gxbase, path)
@@ -141,38 +169,42 @@ func listLinkedPackages() ([][]string, error) {
 	return links, nil
 }
 
-// gx get $hash
-// go get $dvcsimport
-// rm -rf $GOPATH/src/gx/ipfs/$hash/$pkgname
-// ln -s $GOPATH/src/$dvcsimport $GOPATH/src/gx/ipfs/$hash/$pkgname
-// cd $GOPATH/src/$dvcsimport && gx install && gx-go rewrite
-func linkPackage(hash string) (string, error) {
-	srcdir, err := gx.InstallPath("go", "", true)
+// Link the dependency package `dep` to the parent package located in
+// `parentPackagePath` through its `vendor/` directory.
+//
+// The dependency is first fetched to find its
+// DVCS import path (`gx get`), then the repository is fetched through
+// `go get` and linked (respecting the path of the Gx global workspace
+// to use the same rewrite process of `gx-go rw`):
+//   `vendor/gx/ipfs/<pkg-hash>/<pkg-name/` ->  `$GOPATH/src/<dvcs-import>`
+//                              (`target`)  ->   (`linkPath`)
+func linkDependency(dep *gx.Dependency, parentPackagePath string) (string, error) {
+	gxSrcDir, err := gx.InstallPath("go", "", true)
 	if err != nil {
 		return "", err
 	}
-	gxdir := filepath.Join(srcdir, "gx", "ipfs", hash)
 
-	gxget := exec.Command("gx", "get", hash, "-o", gxdir)
-	gxget.Stdout = os.Stderr
-	gxget.Stderr = os.Stderr
-	if err = gxget.Run(); err != nil {
-		return "", fmt.Errorf("error during gx get: %s", err)
-	}
-
-	var pkg gx.Package
-	err = gx.FindPackageInDir(&pkg, gxdir)
+	dvcsImport, err := findDepDVCSimport(dep, gxSrcDir)
 	if err != nil {
-		return "", fmt.Errorf("error during gx.FindPackageInDir: %s", err)
+		return "", fmt.Errorf("error trying to get the DVCS import" +
+			"of the dependeny %s: %s", dep.Name, err)
 	}
 
-	dvcsimport := GxDvcsImport(&pkg)
-	target := filepath.Join(srcdir, dvcsimport)
-	gxtarget := filepath.Join(gxdir, pkg.Name)
+	target := filepath.Join(gxSrcDir, dvcsImport)
+
+	vendorDir := filepath.Join(parentPackagePath, "vendor")
+	os.MkdirAll(filepath.Join(vendorDir, "gx", "ipfs", dep.Hash), os.ModePerm)
+	linkPath := filepath.Join(vendorDir, "gx", "ipfs", dep.Hash, dep.Name)
+	// TODO: Encapsulate paths in a function call (or global setting).
+
+	// Linked package directory, needed for the `post-install` hook.
+	linkPackageDir := filepath.Join(vendorDir, "gx", "ipfs", dep.Hash)
+	// TODO: this shouldn't be necessary, we should be able to just pass the
+	// `linkPath` (i.e., the directory with the name of the package).
 
 	_, err = os.Stat(target)
 	if os.IsNotExist(err) {
-		goget := exec.Command("go", "get", dvcsimport+"/...")
+		goget := exec.Command("go", "get", dvcsImport+"/...")
 		goget.Stdout = nil
 		goget.Stderr = os.Stderr
 		if err = goget.Run(); err != nil {
@@ -182,12 +214,12 @@ func linkPackage(hash string) (string, error) {
 		return "", fmt.Errorf("error during os.Stat: %s", err)
 	}
 
-	err = os.RemoveAll(gxtarget)
+	err = os.RemoveAll(linkPath)
 	if err != nil {
 		return "", fmt.Errorf("error during os.RemoveAll: %s", err)
 	}
 
-	err = os.Symlink(target, gxtarget)
+	err = os.Symlink(target, linkPath)
 	if err != nil {
 		return "", fmt.Errorf("error during os.Symlink: %s", err)
 	}
@@ -200,7 +232,7 @@ func linkPackage(hash string) (string, error) {
 		return "", fmt.Errorf("error during gx install: %s", err)
 	}
 
-	rwcmd := exec.Command("gx-go", "hook", "post-install", gxdir)
+	rwcmd := exec.Command("gx-go", "hook", "post-install", linkPackageDir)
 	rwcmd.Dir = target
 	rwcmd.Stdout = os.Stdout
 	rwcmd.Stderr = os.Stderr
@@ -211,35 +243,46 @@ func linkPackage(hash string) (string, error) {
 	return target, nil
 }
 
-// rm -rf $GOPATH/src/gx/ipfs/$hash
-// gx get $hash
-func unlinkPackage(hash string) (string, error) {
-	srcdir, err := gx.InstallPath("go", "", true)
-	if err != nil {
-		return "", err
-	}
-	gxdir := filepath.Join(srcdir, "gx", "ipfs", hash)
+// Return the DVCS import path of a dependency (fetching it
+// if necessary).
+func findDepDVCSimport(dep *gx.Dependency, gxSrcDir string) (string, error) {
+	gxdir := filepath.Join(gxSrcDir, "gx", "ipfs", dep.Hash)
 
-	err = os.RemoveAll(gxdir)
-	if err != nil {
-		return "", fmt.Errorf("error during os.RemoveAll: %s", err)
-	}
-
-	gxget := exec.Command("gx", "get", hash, "-o", gxdir)
-	gxget.Stdout = nil
+	// Get the dependency to find out its DVCS import.
+	gxget := exec.Command("gx", "get", dep.Hash, "-o", gxdir)
+	gxget.Stdout = os.Stderr
 	gxget.Stderr = os.Stderr
-	if err = gxget.Run(); err != nil {
+	if err := gxget.Run(); err != nil {
 		return "", fmt.Errorf("error during gx get: %s", err)
 	}
 
 	var pkg gx.Package
-	err = gx.FindPackageInDir(&pkg, gxdir)
+	err := gx.FindPackageInDir(&pkg, gxdir)
 	if err != nil {
 		return "", fmt.Errorf("error during gx.FindPackageInDir: %s", err)
 	}
 
-	dvcsimport := GxDvcsImport(&pkg)
-	target := filepath.Join(srcdir, dvcsimport)
+	return GxDvcsImport(&pkg), nil
+}
+
+// rm -rf $GOPATH/src/gx/ipfs/$hash
+// gx get $hash
+func unlinkDependency(dep *gx.Dependency, parentPackagePath string) (string, error) {
+	gxSrcDir, err := gx.InstallPath("go", "", true)
+	if err != nil {
+		return "", err
+	}
+	dvcsImport, err := findDepDVCSimport(dep, gxSrcDir)
+	if err != nil {
+		return "", fmt.Errorf("error trying to get the DVCS import of the dependeny %s: %s", dep.Name, err)
+	}
+
+	err = os.RemoveAll(filepath.Join(parentPackagePath, "vendor", "gx", "ipfs", dep.Hash))
+	if err != nil {
+		return "", fmt.Errorf("error during os.RemoveAll: %s", err)
+	}
+
+	target := filepath.Join(gxSrcDir, dvcsImport)
 
 	uwcmd := exec.Command("gx-go", "uw")
 	uwcmd.Dir = target
@@ -249,7 +292,28 @@ func unlinkPackage(hash string) (string, error) {
 		return "", fmt.Errorf("error during gx-go uw: %s", err)
 	}
 
+	// If `vendor/gx/ipfs/` is empty remove it.
+	if dirEntries(filepath.Join(parentPackagePath, "vendor", "gx", "ipfs")) == 0 &&
+		dirEntries(filepath.Join(parentPackagePath, "vendor", "gx")) == 1 &&
+			dirEntries(filepath.Join(parentPackagePath, "vendor")) == 1 {
+
+		err = os.RemoveAll(filepath.Join(parentPackagePath, "vendor"))
+		if err != nil {
+			return "", fmt.Errorf("error during os.RemoveAll: %s", err)
+		}
+	}
+	// TODO: This check could be done in a more elegant way.
+
 	return target, nil
+}
+
+// Return the number of entries in the directory (mask errors as 0).
+func dirEntries(name string) int {
+	entries, err := ioutil.ReadDir(name)
+	if err != nil {
+		return 0
+	}
+	return len(entries)
 }
 
 func GxDvcsImport(pkg *gx.Package) string {
